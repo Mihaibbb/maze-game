aa603c65c6d0f4ff3c00eab892ab75da9242b671
Mihaibbb <79415664+Mihaibbb@users.noreply.github.com>
2021-08-07 10:20:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const game = document.querySelector('.game');
+const board = document.querySelector('.board');
+
+
+board.style.height = `${board.clientWidth}px`;
+
+window.addEventListener('resize', () => {
+
+});
